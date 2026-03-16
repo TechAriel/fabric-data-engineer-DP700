25afa14bbccfa4eb2adfaeca4d95be3a5bce6d9a
Gabriel Oduor <91619174+TechAriel@users.noreply.github.com>
2026-03-16 10:53:04 +0100

    Update sql_queries.sql

--- 04-fabric-eventhouse-kql-analytics/Queries/sql_queries.sql
+++ 04-fabric-eventhouse-kql-analytics/Queries/sql_queries.sql
@@ -1,1 +1,55 @@
+// Transact-SQL
 
+// Retrieving data
+
+SELECT TOP 100 *
+FROM Bikestream
+
+// retrieving specific columns
+
+SELECT TOP 10 Street, No_Bikes
+FROM Bikestream
+
+// assigning an alias that renames No_Empty_Docks 
+
+SELECT TOP 10 Street, No_Empty_Docks as ["Number of Empty Docks"]
+FROM Bikestream
+
+// Summarization -- query to find the total number of bikes available
+
+SELECT sum(No_Bikes) as [Total Number of Bikes]
+FROM Bikestream
+
+// query to group the total number of bikes by neighbourhood
+
+SELECT Neighbourhood, sum(No_Bikes) as [Total Number of Bikes]
+FROM Bikestream
+GROUP BY Neighbourhood
+
+// CASE statement to group bike points with an unknown origin into a "Unidentified" category for follow-up
+
+SELECT CASE
+         WHEN Neighbourhood IS NULL OR Neighbourhood = "" THEN 'Unidentified' ELSE Neighbourhood
+       END AS Neighbourhood,
+       sum(No_Bikes) as [Total Number of Bikes]
+FROM Bikestream
+GROUP BY Neighbourhood
+
+// query to order the grouped results by neighbourhood
+
+SELECT Neighbourhood,
+        sum(No_Bikes) AS [Total Number of Bikes] 
+FROM Bikestream
+GROUP BY Neighbourhood 
+ORDER BY Neighbourhood ASC  
+
+// query to filter the grouped data so that only rows having a neighbourhood of “Chelsea” are included in the results
+
+SELECT Neighbourhood,
+        sum(No_Bikes) AS [Total NUmber of Bikes]
+FROM Bikestream
+GROUP BY Neighbourhood
+HAVING Neighbourhood = 'Chelsea'
+ORDER BY Neighbourhood ASC
+
+    
