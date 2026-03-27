54b3f8b49a7447709bd923c0b8ba3202039fd9cc
Gabriel Oduor <91619174+TechAriel@users.noreply.github.com>
2026-03-27 14:16:04 +0100

    Update sql_queries.sql

--- 12-fabric-data-warehouse-analytics/queries/sql_queries.sql
+++ 12-fabric-data-warehouse-analytics/queries/sql_queries.sql
@@ -1,1 +1,58 @@
+-- Create product table
+CREATE TABLE dbo.DimProduct
+(
+    ProductKey INTEGER NOT NULL,
+    ProductAltKey VARCHAR(25) NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Category VARCHAR(50) NULL,
+    ListPrice DECIMAL(5,2) NULL
+);
+GO
 
+-- Insert sample data
+INSERT INTO dbo.DimProduct
+VALUES
+(1, 'RING1', 'Bicycle bell', 'Accessories', 5.99),
+(2, 'BRITE1', 'Front light', 'Accessories', 15.49),
+(3, 'BRITE2', 'Rear light', 'Accessories', 15.49);
+GO
+
+-- Sales aggregation by time
+SELECT  d.[Year] AS CalendarYear,
+        d.[Month] AS MonthOfYear,
+        d.MonthName,
+        SUM(so.SalesTotal) AS SalesRevenue
+FROM FactSalesOrder AS so
+JOIN DimDate AS d ON so.SalesOrderDateKey = d.DateKey
+GROUP BY d.[Year], d.[Month], d.MonthName
+ORDER BY CalendarYear, MonthOfYear;
+
+-- Sales by region
+SELECT  d.[Year] AS CalendarYear,
+        d.[Month] AS MonthOfYear,
+        d.MonthName,
+        c.CountryRegion,
+        SUM(so.SalesTotal) AS SalesRevenue
+FROM FactSalesOrder AS so
+JOIN DimDate AS d ON so.SalesOrderDateKey = d.DateKey
+JOIN DimCustomer AS c ON so.CustomerKey = c.CustomerKey
+GROUP BY d.[Year], d.[Month], d.MonthName, c.CountryRegion
+ORDER BY CalendarYear, MonthOfYear, CountryRegion;
+
+-- Create view
+CREATE VIEW vSalesByRegion
+AS
+SELECT  d.[Year] AS CalendarYear,
+        d.[Month] AS MonthOfYear,
+        d.MonthName,
+        c.CountryRegion AS SalesRegion,
+        SUM(so.SalesTotal) AS SalesRevenue
+FROM FactSalesOrder AS so
+JOIN DimDate AS d ON so.SalesOrderDateKey = d.DateKey
+JOIN DimCustomer AS c ON so.CustomerKey = c.CustomerKey
+GROUP BY d.[Year], d.[Month], d.MonthName, c.CountryRegion;
+
+-- Query view
+SELECT CalendarYear, MonthName, SalesRegion, SalesRevenue
+FROM vSalesByRegion
+ORDER BY CalendarYear, MonthOfYear, SalesRegion;
